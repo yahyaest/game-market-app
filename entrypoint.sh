602c324a0dfa6381d756a0c48ce1366fe1522b6f
yahya <yahyaest1000@gmail.com>
2023-10-22 20:05:14 +0100

    First Commit

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Retrieve the IP address of the gateway, comment services from DNS resolution
+gateway_ip=$(getent hosts gateway | awk '{ print $1 }')
+# comment_ip=$(getent hosts comment | awk '{ print $1 }')
+# front_ip=$(getent hosts front | awk '{ print $1 }')
+
+
+
+# Export the IP addresses as environment variables
+
+export GATEWAY_BASE_URL=http://$gateway_ip:4000
+# export COMMENT_BASE_URL=http://$comment_ip:8000
+# export FRONT_BASE_URL=http://$front_ip:3000
+
+env
+
+npm run dev
+
+exec "$@"
