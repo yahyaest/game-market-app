758197843c1ff329c801d4f0aea392431c743628
yahya <yahyaest1000@gmail.com>
2023-12-30 20:50:26 +0100

    Add page selection for pagination component

--- components/gamesPage/pagination.tsx
+++ components/gamesPage/pagination.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { Pagination } from "@nextui-org/react";
+import { Button, Input, Pagination } from "@nextui-org/react";
 
 type Props = {
   getGames: (page: number) => Promise<any>;
@@ -15,6 +15,7 @@ export default function PaginationComponent({
 }: Props) {
   const [pageNumber, setPageNumber] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPage, setSelectedPage] = useState(1);
   const [nextPageUrl, setNextPageUrl] = useState(null);
   const [previousPageUrl, setPreviousPageUrl] = useState(null);
 
@@ -32,22 +33,49 @@ export default function PaginationComponent({
   }, []);
 
   return (
-    <Pagination
-      className="my-5"
-      classNames={{
-        item: "text-[0.6rem] font-semibold",
-      }}
-      showControls
-      total={pageNumber ? pageNumber : 10}
-      initialPage={1}
-      onChange={async (page: number) => {
-        setCurrentPage(page);
-        const data = await getGames(page);
-        setCurrentGames(data.results);
-        setPageNumber(Math.floor(data.count / data.results.length));
-        setNextPageUrl(data.next);
-        setPreviousPageUrl(data.previous);
-      }}
-    />
+    <div className="flex flex-row items-center">
+      <Pagination
+        className="my-5"
+        classNames={{
+          item: "text-[0.6rem] font-semibold",
+        }}
+        showControls
+        total={pageNumber ? pageNumber : 10}
+        page={currentPage}
+        initialPage={1}
+        onChange={async (page: number) => {
+          setCurrentPage(page);
+          const data = await getGames(page);
+          setCurrentGames(data.results);
+          setNextPageUrl(data.next);
+          setPreviousPageUrl(data.previous);
+        }}
+      />
+      <Button
+        className="mx-2"
+        radius="full"
+        color="primary"
+        size="sm"
+        onClick={async () => {
+          setCurrentPage(selectedPage);
+          const data = await getGames(selectedPage);
+          setCurrentGames(data.results);
+          setNextPageUrl(data.next);
+          setPreviousPageUrl(data.previous);
+        }}
+      >
+        Go To Page
+      </Button>
+      <Input
+        className="w-20 mx-2"
+        size="sm"
+        type="number"
+        min={1}
+        max={pageNumber}
+        onChange={(e) => {
+          setSelectedPage(+e.target.value);
+        }}
+      />
+    </div>
   );
 }
