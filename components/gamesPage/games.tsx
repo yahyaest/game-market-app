16e642a41366867941dd6d9be48b9e42601fa662
yahya <yahyaest1000@gmail.com>
2024-03-03 20:37:29 +0100

    Fix missing data in games components

--- components/gamesPage/games.tsx
+++ components/gamesPage/games.tsx
@@ -55,70 +55,79 @@ export default function GamesPage({
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-16">
       <div className="grid grid-cols-1 gap-6  md:grid-cols-2 lg: lg:grid-cols-3 xl:grid-cols-4">
-        {currentGames.map((game: any, index: number) => (
-          <div
-            key={game.id}
-            onClick={() =>
-              pageComponent === "games"
-                ? router.push(`/games/${game.slug}/${game.id}`)
-                : router.push(`/store/${game.slug}`)
-            }
-          >
-            <Card
-              className={`${
-                bgColors[index % bgColors.length]
-              } py-4 mx-auto w-80 cursor-pointer hover:scale-105`}
+        {currentGames ? (
+          currentGames.map((game: any, index: number) => (
+            <div
+              key={game.id}
+              onClick={() =>
+                pageComponent === "games"
+                  ? router.push(`/games/${game.slug}/${game.id}`)
+                  : router.push(`/store/${game.slug}`)
+              }
             >
-              <CardHeader className="pb-0 pt-2 px-4 flex-col">
-                <div className="grid grid-cols-2 gap-20 justify-items-stretch align-items-center">
-                  <div className="justify-self-start flex flex-raw gap-2 pt-2 text-tiny uppercase font-bold">
-                    {renderGamePlatform(
-                      game.parent_platforms || game.external_args.platforms
-                    )}
+              <Card
+                className={`${
+                  bgColors[index % bgColors.length]
+                } py-4 mx-auto w-80 cursor-pointer hover:scale-105`}
+              >
+                <CardHeader className="pb-0 pt-2 px-4 flex-col">
+                  <div className="grid grid-cols-2 gap-20 justify-items-stretch align-items-center">
+                    <div className="justify-self-start flex flex-raw gap-2 pt-2 text-tiny uppercase font-bold">
+                      {renderGamePlatform(
+                        game.parent_platforms || game.external_args.platforms
+                      )}
+                    </div>
+                    <div className="justify-self-end pb-2">
+                      {(game.metacritic || game.external_args?.metacritic) && (
+                        <Chip color="warning" variant="shadow">
+                          {game.metacritic || game.external_args.metacritic}
+                        </Chip>
+                      )}
+                    </div>
                   </div>
-                  <div className="justify-self-end pb-2">
-                    {(game.metacritic || game.external_args?.metacritic) && (
-                      <Chip color="warning" variant="shadow">
-                        {game.metacritic || game.external_args.metacritic}
-                      </Chip>
-                    )}
-                  </div>
-                </div>
-                <h4
-                  className={`${
-                    (game.name || game.title).length <= 20
-                      ? "font-bold text-large"
-                      : (game.name || game.title).length <= 30
-                      ? "font-bold text-[0.85rem]"
-                      : (game.name || game.title).length <= 45
-                      ? "font-bold text-[0.8rem]"
-                      : "font-bold text-[0.7rem]"
-                  } h-8 `}
-                >
-                  {game.name || game.title}
-                </h4>
-              </CardHeader>
-              <CardBody className="overflow-visible py-2 mx-3">
-                <Image
-                  alt={game.name || game.title}
-                  className="object-cover rounded-xl h-36"
-                  src={
-                    game.background_image || game.external_args.background_image
-                  }
-                  width={270}
-                />
-              </CardBody>
-            </Card>
+                  <h4
+                    className={`${
+                      (game.name || game.title).length <= 20
+                        ? "font-bold text-large"
+                        : (game.name || game.title).length <= 30
+                        ? "font-bold text-[0.85rem]"
+                        : (game.name || game.title).length <= 45
+                        ? "font-bold text-[0.8rem]"
+                        : "font-bold text-[0.7rem]"
+                    } h-8 `}
+                  >
+                    {game.name || game.title}
+                  </h4>
+                </CardHeader>
+                <CardBody className="overflow-visible py-2 mx-3">
+                  <Image
+                    alt={game.name || game.title}
+                    className="object-cover rounded-xl h-36"
+                    src={
+                      game.background_image ||
+                      game.external_args.background_image
+                    }
+                    width={270}
+                  />
+                </CardBody>
+              </Card>
+            </div>
+          ))
+        ) : (
+          <div className="text-center text-amber-600 text-3xl font-bold">
+            Failed to load store games
           </div>
-        ))}
+        )}
       </div>
-      <PaginationComponent
-        serverGamesResponse={serverGamesResponse}
-        gameCount={gameCount}
-        getGames={getGames}
-        setCurrentGames={setCurrentGames}
-        pageComponent={pageComponent}
-      />
+      {currentGames && (
+        <PaginationComponent
+          serverGamesResponse={serverGamesResponse}
+          gameCount={gameCount}
+          getGames={getGames}
+          setCurrentGames={setCurrentGames}
+          pageComponent={pageComponent}
+        />
+      )}
     </div>
   );
 }
